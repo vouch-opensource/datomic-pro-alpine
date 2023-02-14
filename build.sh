3c308de35d3d934e1f4d5fdb4c5e72efba8d7a3a
MatheusAfinovicz <matheusafinovicz@gmail.com>
2023-02-14 08:31:22 -0300

    test: build multiplatform images

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ DOCKER_TAG=${IMAGE}:${VERSION}
 
 case "$1" in
   build)
-    DOCKER_BUILDKIT=1 docker buildx build -t ${DOCKER_TAG} \
+    docker buildx build -t ${DOCKER_TAG} \
       --platform linux/amd64 \
       --platform linux/x86_64 \
       --platform linux/arm64/v8 \
